55b47574c9df5fd8f7945cfb138aa2faa44752d6
James Corfe <james.corfe@gmail.com>
2023-08-22 16:57:13 +0100

    price alert added

--- stock_monitor.py
+++ stock_monitor.py
@@ -32,3 +32,37 @@ def get_historical_data(symbol, range='1m'):
 def calculate_7_day_average(data):
     closing_prices = [entry['close'] for entry in data[-7:]]
     return sum(closing_prices) / 7
+
+    previous_prices = {}
+
+# Get stock prices
+stock_prices = get_stock_prices(stock_symbols)
+
+# Get 7-day moving averages
+seven_day_averages = {}
+for symbol in stock_symbols:
+    historical_data = get_historical_data(symbol)
+    seven_day_average = calculate_7_day_average(historical_data)
+    seven_day_averages[symbol] = seven_day_average
+
+def check_price_alerts(current_prices, previous_prices, threshold=0.25):
+    alerts = []
+    for symbol in current_prices:
+        if symbol in previous_prices:
+            price_diff = previous_prices[symbol] - current_prices[symbol]
+            if price_diff >= threshold:
+                alerts.append(symbol)
+        previous_prices[symbol] = current_prices[symbol]
+    return alerts
+
+# Get current stock prices
+current_prices = get_stock_prices(stock_symbols)
+
+# Check price alerts
+alerts = check_price_alerts(current_prices, previous_prices)
+
+# Print alerts
+if alerts:
+    print(f"Price alerts triggered for: {', '.join(alerts)}")
+else:
+    print("No price alerts triggered.")
