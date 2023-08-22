41af228196e60d0f66abb3aa6d5debb20bb1b935
James Corfe <james.corfe@gmail.com>
2023-08-22 16:54:40 +0100

    7 day Average function added

--- stock_monitor.py
+++ stock_monitor.py
@@ -14,3 +14,21 @@ def get_stock_prices(symbols):
         data = response.json()
         stock_prices[symbol] = data['latestPrice']
     return stock_prices
+
+# Get stock prices
+stock_prices = get_stock_prices(stock_symbols)
+
+# Print stock prices
+for symbol, price in stock_prices.items():
+    print(f'Current price of {symbol}: ${price}')
+
+
+def get_historical_data(symbol, range='1m'):
+    url = f'https://cloud.iexapis.com/stable/stock/{symbol}/chart/{range}?token={API_TOKEN}'
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+def calculate_7_day_average(data):
+    closing_prices = [entry['close'] for entry in data[-7:]]
+    return sum(closing_prices) / 7
