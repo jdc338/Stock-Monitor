b502dbdc95cf33c2fa81fe140e9785613e910806
James Corfe <james.corfe@gmail.com>
2023-08-23 09:25:07 +0100

    Adding Documentation

--- stock_monitor.py
+++ stock_monitor.py
@@ -1,42 +1,40 @@
 import requests
 
-# Replace 'YOUR_API_TOKEN' with your actual IEX Cloud API token
+# IEX Cloud API token
 API_TOKEN = 'pk_e707832aae934f04b8d2bff18da0583f'
 
 # List of stock symbols to monitor
 stock_symbols = ['TSLA', 'AAPL', 'MSFT', 'GOOGL', 'NKE']
 
-def get_stock_prices(symbols):
-    stock_prices = {}
-    for symbol in symbols:
-        url = f'https://cloud.iexapis.com/stable/stock/{symbol}/quote?token={API_TOKEN}'
-        response = requests.get(url)
-        data = response.json()
-        stock_prices[symbol] = data['latestPrice']
-    return stock_prices
 
-# Get stock prices
+def get_stock_prices(symbols):
+    stock_prices = {} # Initialize an empty dictionary to store stock prices
+    for symbol in symbols: # Loop through each symbol in the list 'symbols'
+        url = f'https://cloud.iexapis.com/stable/stock/{symbol}/quote?token={API_TOKEN}' # Construct the URL for the API request by formatting the symbol and API token
+        response = requests.get(url) # Send an HTTP GET request to the API endpoint
+        data = response.json() # Parse the JSON content of the response into a Python dictionary
+        stock_prices[symbol] = data['latestPrice'] # Retrieve the 'latestPrice' field from the parsed JSON data and store it in the dictionary
+    return stock_prices # Return the dictionary containing stock prices for each symbol
+
+# Get stock prices for specificed symbols
 stock_prices = get_stock_prices(stock_symbols)
 
 # Print stock prices
 for symbol, price in stock_prices.items():
     print(f'Current price of {symbol}: ${price}')
 
-
-def get_historical_data(symbol, range='1m'):
+# This line defines a function named get_historical_data that takes two parameters: symbol and range
+def get_historical_data(symbol, range='1m'): #The range parameter specifies the time range for the historical data. It has a default value of '1m', which represents the past month.
     url = f'https://cloud.iexapis.com/stable/stock/{symbol}/chart/{range}?token={API_TOKEN}'
     response = requests.get(url)
     data = response.json()
     return data
 
 def calculate_7_day_average(data):
-    closing_prices = [entry['close'] for entry in data[-7:]]
-    return sum(closing_prices) / 7
+    closing_prices = [entry['close'] for entry in data[-7:]] # Create a list of the 'close' values from the last 7 entries in the 'data' list
+    average = sum(closing_prices) / 7 # Calculate the sum of closing prices and return the average by dividing by 7
+    print(f'Current average for {symbol}: ${average}')
 
-previous_prices = {}
-
-# Get stock prices
-stock_prices = get_stock_prices(stock_symbols)
 
 # Get 7-day moving averages
 seven_day_averages = {}
@@ -44,6 +42,8 @@ for symbol in stock_symbols:
     historical_data = get_historical_data(symbol)
     seven_day_average = calculate_7_day_average(historical_data)
     seven_day_averages[symbol] = seven_day_average
+
+previous_prices = {}
 
 def check_price_alerts(current_prices, previous_prices, threshold=0.25):
     alerts = []
